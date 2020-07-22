4fa3689a2c693798975ffff136452704957aa983
Bernardo de Oliveira <oliveira.bernardocb@gmail.com>
2020-07-21 22:23:30 -0300

    Add Meeple class

---
+++ src/domain/model/meeple.ts
@@ -0,0 +1,9 @@
+import { TurnOrder } from './player';
+import { TileFeature } from './feature/tileFeature';
+
+export default class Meeple {
+
+    playerTurn!: TurnOrder;
+
+    tileFeature!: TileFeature;
+}
